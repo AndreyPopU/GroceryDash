7581d1dc05ec68ef82822dc260e39914776f8ee7
Andrey Popov <andreypopu@gmail.com>
2023-03-29 15:09:57 +0200

    Imported models and made some changes to the checkouts

--- Assets/Scripts/Checkout.cs
+++ Assets/Scripts/Checkout.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Checkout : MonoBehaviour
 {
-    
+    public bool open = true;
+    public Material openMat, closedMat;
+
+    private MeshRenderer lights;
+
+    private void Start()
+    {
+        lights = GetComponentInChildren<MeshRenderer>();
+        lights.material = openMat;
+
+        InvokeRepeating("ChangeColor", 3, 3);
+    }
+
+    private void ChangeColor()
+    {
+        open = !open;
+
+        if (open) lights.material = openMat;
+        else lights.material = closedMat;
+    }
+
 }
